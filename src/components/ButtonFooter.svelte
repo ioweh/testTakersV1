d3ce5c8ad5923798985aac00e1d2b63685ab6788
Oleg Ilinets <oleg.ilinets@rokolabs.com>
2020-06-14 08:17:03 +0300

    Fixed tests

--- src/components/ButtonFooter.svelte
+++ src/components/ButtonFooter.svelte
@@ -12,6 +12,5 @@
 </script>
 
 <button class="link" tabindex="0" on:click={handleClick}>
-  <i class="fa fa-eye" aria-hidden="true" />
   <span>View details</span>
 </button>
--- tests/unit/main.spec.js
+++ tests/unit/main.spec.js
@@ -1,13 +1,50 @@
 // NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
 import '@testing-library/jest-dom/extend-expect'
+import { getTestTakerAction, getTestTakersAction } from "../../src/store";
 
-import { render } from '@testing-library/svelte'
+var mockedTestTaker = {
+  "userId": "1",
+  "login": "grahamallison",
+  "password": "steel",
+  "title": "ms",
+  "lastName": "graham",
+  "firstName": "allison",
+  "gender": "female",
+  "email": "allison.graham70@example.com",
+  "picture": "https://randomuser.me/api/portraits/women/35.jpg",
+  "address": "6697 rolling green rd colorado springs 56306"
+},
+  mockedTestTakers = [
+    {
+      "userId": 0,
+      "firstName": "abigail",
+      "lastName": "foster"
+    },
+    {
+      "userId": 14,
+      "firstName": "abigail",
+      "lastName": "edwards"
+    }];
 
-import App from '../../src/App.svelte'
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(
+      mockedTestTaker
+    ),
+    status: 200
+  })
+);
 
-test('shows proper heading when rendered', () => {
-  const { getByText } = render(App, { name: 'World' })
-
-  expect(getByText('Hello World!')).toBeInTheDocument()
+test('a test taker should be received', async () => {
+  await expect(getTestTakerAction(1)).resolves.toEqual(mockedTestTaker);
 })
 
+test('a list of test takers should be received', async () => {
+  fetch.mockImplementationOnce(() => Promise.resolve({
+    json: () => Promise.resolve(
+      mockedTestTakers
+    ),
+    status: 200
+  }));
+  await expect(getTestTakersAction()).resolves.toEqual(mockedTestTakers);
+})
